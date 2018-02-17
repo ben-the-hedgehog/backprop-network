09746246224022c6bcd8a2aa09b84dbfdbce69c3
bennyp <benpang@outlook.com>
2018-02-16 20:10:52 -0500

    classification functionalities

--- network.py
+++ network.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class bp_network(object):
+class bp_classifer(object):
 
 	def __init__(self, layers, learning_rate, momentum):
 		"""
@@ -33,16 +33,35 @@ class bp_network(object):
 			self.weights.append(weight_mtx)
 			self.biases.append(bias_vector)
 
+	def one_hot(self, d):
+		"""transforms a numeric label into one-hot column vector"""
+		y = np.zeros((self.num_outputs, 1))
+		y[d] = 1
+		return y
+
+	def train(self, train_X, train_D, val_X=None, val_D=None, num_epochs=1):
+
+		train_Y = list(map(self.one_hot, train_D))
+		val_Y = list(map(self.one_hot, val_D)) if val_D else None
+		error = []
+
+		for epoch in range(num_epochs):
+
+			for x, y in zip(train_X, train_Y):
+				self.update_SGD(x.reshape(1, self.num_inputs), y)
+
+
 	def update_SGD(self, x, y):
 		"""
 		Applies a single step of stochastic gradient descent
 
 		:param x:<np.arrray> 1 x m feature vector from dataset
-		:param y:<double> numeric target
+		:param y:<np.array> one-hot target vector indicating desired label
 		"""
+
 		del_w, del_b = self.backpropogate(x, y)
 		self.weights = [w - self.c * dw + self.alpha * w for w, dw in zip(self.weights, del_w)]
-		self.biases = [b - self.c * db + self.alpha * b for b, db in zip(self.weights, del_b)]
+		self.biases = [b - self.c * db + self.alpha * b for b, db in zip(self.biases, del_b)]
 
 		return None
 
@@ -52,8 +71,9 @@ class bp_network(object):
 		and biases in each layer
 
 		:param x:<np.arrray> 1 x m feature vector from dataset
-		:param y:<double> numeric target
+		:param y:<np.array> one-hot target vector indicating desired label
 		"""
+
 		# These will hold the gradient of the cost function with respect to
 		# weights and biases
 		del_w = [np.zeros(w.shape) for w in self.weights]
@@ -71,7 +91,8 @@ class bp_network(object):
 			activations.append(activation)
 
 		# Error at output layer
-		delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(net_inputs[-1]) #dC/dz at output
+		output = self.one_hot(np.argmax(activations[-1]))
+		delta = self.cost_derivative(output, y) * sigmoid_prime(net_inputs[-1]) #dC/dz at output
 		del_b[-1] = delta #dC/db = dC/dz
 		del_w[-1] = np.dot(delta, activations[-2].T)
 
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def init_test():
-	net = network.bp_network([3, 2, 3, 1], 0.1, 0.01)
+	net = network.bp_classifer([3, 2, 3, 1], 0.1, 0.01)
 	assert net.num_layers == 4
 	assert net.num_inputs == 3
 	assert net.num_outputs == 1
@@ -15,31 +15,53 @@ def init_test():
 	pass
 
 def backprop_test():
-	net = network.bp_network([3, 2, 3, 1], 0.1, 0.01)
+	net = network.bp_classifer([3, 2, 3, 3], 0.1, 0.01)
 	x = np.array([1, 3, 0]).reshape(1, 3)
-	y = np.array([6]).reshape(1, 1)
+	#y = np.array([2]).reshape(1, 1)
+	y = net.one_hot(2)
 	dw, db = net.backpropogate(x, y)
 	weight_shapes = [w.shape for w in dw]
-	assert weight_shapes == [(2, 3), (3, 2), (1, 3)]
+	assert weight_shapes == [(2, 3), (3, 2), (3, 3)]
 	bias_shapes = [b.shape for b in db]
-	assert bias_shapes == [(2, 1), (3, 1), (1, 1)]
+	assert bias_shapes == [(2, 1), (3, 1), (3, 1)]
 	print("backprop_test passed!\n")
 	pass
 
 def update_test():
-	net = network.bp_network([3, 2, 3, 1], 0.1, 0.01)
+	net = network.bp_classifer([3, 2, 3], 0.1, 0.01)
 	x = np.array([1, 3, 0]).reshape(1, 3)
-	y = np.array([6]).reshape(1, 1)
+	#y = np.array([0]).reshape(1, 1)
+	y = net.one_hot(0)
 	w = net.weights.copy()
 	b = net.biases.copy()
 	net.update_SGD(x, y)
 	for i in range(len(w)):
-		assert np.array_equal(net.weights[i], w[i]) is False
-		assert np.array_equal(net.biases[i], b[i]) is False
+		assert net.weights[i].shape == w[i].shape
+		assert net.biases[i].shape == b[i].shape
 	print("update_test passed!\n")
+	pass
+
+def train_test():
+	net = network.bp_classifer([3, 2, 3], 0.1, 0.01)
+	print("BEFORE TRAINING\n")
+	for w, b in zip(net.weights, net.biases):
+		print(w)
+		print(b)
+
+	train_X = np.random.randint(10, size=(10, 3))
+	train_D = np.random.randint(3, size=(10, 1))
+	net.train(train_X, train_D)
+
+	print("AFTER TRAINING\n")
+	for w, b in zip(net.weights, net.biases):
+		print(w)
+		print(b)
+		
+	print("train_test passed!\n")
 	pass
 
 if __name__ == '__main__':
 	init_test()
 	backprop_test()
 	update_test()
+	train_test()
